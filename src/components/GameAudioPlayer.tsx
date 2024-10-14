ac771ba84f566169aa9757421a67a6fa64c2b443
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2024-10-15 00:43:41 +0200

    feat(game):  small style changes

--- src/components/GameAudioPlayer.tsx
+++ src/components/GameAudioPlayer.tsx
@@ -29,10 +29,10 @@ export default function GameAudioPlayer() {
     }
 
     return (
-        <div className="fixed top-4 right-4 backdrop-grayscale">
+        <div className="fixed top-4 right-4 backdrop-grayscale z-50">
             <button id='muteButton'
                 onClick={toggleMute}
-                className="w-12 h-12 rounded-full bg-gray-800 text-white hover:bg-gray-700 transition-colors shadow-lg flex items-center justify-center"
+                className="w-12 h-12 rounded-full bg-gray-850 text-gray-100 hover:bg-gray-700 transition-colors shadow-lg flex items-center justify-center"
             >
                 {isMuted ? <VolumeX className="h-6 w-6 text-gray-400" /> : <Volume2 className="h-6 w-6 text-gray-400" />}
             </button>
