5b317809fabe991e67a3727310cdcd32c49c4392
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2024-10-19 13:16:13 +0200

    feat(header): add sub titles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,17 @@ const Header = () => {
     return (
         <header
             className={` text-gray-100 p-5 shadow-lg absolute transition-opacity duration-500 ${obscure ? 'opacity-10 bg-gray-900' : 'opacity-100 bg-gray-950'}`}>
-            < div className="container mx-auto flex items-center justify-between">
+            <div className="container mx-auto flex  flex-col items-center justify-between">
                 <h1 className="text-3xl font-bold flex items-center">
                     <Skull className="mr-2 text-red-500 w-8 h-8" />
                     Spooky Hack
                 </h1>
+                <p className="text-sm italic text-gray-400 mt-1">
+                    "Where the code haunts you... or not"
+                </p>
+                <p className="text-sm italic text-gray-700 mt-1">
+                    Check the repo → <a href='https://github.com/jesusnoseq/SpookyHackCYOA'>SpookyHackCYOA</a>
+                </p>
             </div>
         </header >
     );
